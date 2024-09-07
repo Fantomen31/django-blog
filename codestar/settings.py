9a83939ba0b06dc00b86c2df5507f96e78543ab5
The_phantom walker <Neocoinkitt@gmail.com>
2024-09-07 22:05:27 +0000

    added html templates + some shit

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-fantomen31-djangoblog-2zc7iwmvp08.ws-eu116.gitpod.io', '.herokuapp.com']
 
