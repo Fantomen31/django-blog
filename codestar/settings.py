8414098371b2bf6cdca02db5f3e47b79a6f20e53
The_phantom walker <Neocoinkitt@gmail.com>
2024-09-10 18:53:30 +0000

    added edit + delete comment

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-fantomen31-djangoblog-2zc7iwmvp08.ws-eu116.gitpod.io', '.herokuapp.com']
 
--- static/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("Yes, this is I");
